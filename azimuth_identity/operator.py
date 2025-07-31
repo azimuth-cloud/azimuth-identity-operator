azimuth-cloud/azimuth-identity-operator

commit 80d0c8e8ee8b1a6dd6c06098be8ba06e8f3fe6ac

Add platform users group to realm status

azimuth_identity/operator.py
@@ -173,6 +173,12 @@ async def reconcile_realm(instance: api.Realm, **kwargs):
     instance.status.phase = api.RealmPhase.READY
     instance.status.oidc_issuer_url = f"{settings.keycloak.base_url}/realms/{realm_name}"
     instance.status.admin_url = f"{settings.keycloak.base_url}/admin/{realm_name}/console"
+    # Keycloak group names are prefixed with a slash
+    instance.status.platform_users_group = (
+        f"/{settings.keycloak.platform_users_group_name}"
+        if instance.spec.tenancy_id
+        else None
+    )
     LOGGER.info("Realm reconciled successfully, saving status - %s", format_instance(instance))
     await save_instance_status(instance)
 
@@ -358,6 +364,16 @@ async def reconcile_platform(instance: api.Platform, param, **kwargs):
             format_instance(instance),
             service_name
         )
+        # Calculate the allowed groups
+        allowed_groups = [
+            # Allow the parent group for the platform
+            group["path"],
+            # Allow users to be added to a subgroup for the specific service
+            subgroup["path"],
+        ]
+        # Add the platform users group if the realm has one
+        if realm.status.platform_users_group:
+            allowed_groups.insert(0, realm.status.platform_users_group)
         await ekclient.apply_object(
             {
                 "apiVersion": "v1",
@@ -379,14 +395,7 @@ async def reconcile_platform(instance: api.Platform, param, **kwargs):
                     "issuer-url": realm.status.oidc_issuer_url,
                     "client-id": client["clientId"],
                     "client-secret": client["secret"],
-                    "allowed-groups": json.dumps([
-                        # We allow the platform users group
-                        f"/{settings.keycloak.platform_users_group_name}",
-                        # Allow the parent group for all services
-                        group["path"],
-                        # Allow users to be added to a subgroup for the specific service
-                        subgroup["path"],
-                    ]),
+                    "allowed-groups": json.dumps(allowed_groups),
                 },
             },
             force = True

azimuth_identity/models/v1alpha1/realm.py
@@ -40,6 +40,10 @@ class RealmStatus(schema.BaseModel, extra = "allow"):
         None,
         description = "The admin URL for the realm."
     )
+    platform_users_group: schema.Optional[schema.constr(min_length = 1)] = Field(
+        None,
+        description = "The name of the platform users group for the realm."
+    )
     failure_message: str = Field(
         "",
         description = "The reason that the realm entered the failed phase, if known."

commit bab8777f85f3ceb6cbdde8bdca0912b49f9668e1

Fix platform 'allowed groups' race condition on operator upgrade
Commit 80d0c8e8ee8b1a6dd6c06098be8ba06e8f3fe6ac introduced a
subtle bug where, upon upgrading the operator to version 0.9.0
using standard Azimuth tooling, the `/platform-users` group is
inadvertently dropped from the allowed groups list for all
platforms deployed prior to the upgrade. This causes HTTP 403
errors in the browser when a user attempts to access any Zenith
service for the previously deployed platform.

The root cause of this bug is as follows:

During an Azimuth upgrade, when the new identity operator chart
gets installed it restarts the identity operator container and
triggers the ‘on resume’ handler for both the Realm and Platform
CRDs. This means that both realms and platforms get reconciled at
the same time so the Platform resources get reconciled before the
operator has added the status.platform_users_group field to the
Realm and this causes /platform-users to be removed from the
allowed groups.

The fix proposed here is to fall back to pre-0.9.0 behaviour if
the realm.status.platform_users_group field is empty. This
preserves backwards compatibility with existing platforms but
could also be removed in a future 0.10.0 release assuming that
by that point all realms have been reconciled at least once using
the 0.9.0 behaviour in which the status field is populated.

azimuth_identity/operator.py
@@ -392,14 +392,16 @@ async def reconcile_platform(instance: api.Platform, param, **kwargs):
         )
         # Calculate the allowed groups
         allowed_groups = [
+            # Add the platform users group if the realm has one
+            realm.status.get(
+                "platform_users_group",
+                f"/{settings.keycloak.platform_users_group_name}",
+            ),
             # Allow the parent group for the platform
             group["path"],
             # Allow users to be added to a subgroup for the specific service
             subgroup["path"],
         ]
-        # Add the platform users group if the realm has one
-        if realm.status.platform_users_group:
-            allowed_groups.insert(0, realm.status.platform_users_group)
         await ekclient.apply_object(
             {
                 "apiVersion": "v1",